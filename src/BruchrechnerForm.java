85f1b9fcbb54212baf875d8b370ec16ed590220d
Sara.kutschi <sara.kutschi@hotmail.com>
2025-10-04 17:24:47 +0200

    Initial commit

---
+++ src/BruchrechnerForm.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class BruchrechnerForm {
+
+    private JPanel HeadPanel;
+    private JLabel titleLbl;
+    private JPanel InputPanel;
+    private JLabel aLbl;
+    private JLabel bLbl;
+    private JComboBox opCb;
+    private JPanel OperatorPanel;
+    private JButton resetButton;
+    private JButton berechnenBtn;
+    private JPanel ActionPanel;
+    private JLabel resBruchLbl;
+    private JTextField resZeahlerTf;
+    private JTextField resNennerTf;
+    private JPanel ResultPanel;
+    private JTextField aZeahlerTf;
+    private JTextField aNennerTf;
+    private JTextField bNennerTf;
+    private JTextField bZeahlerTf;
+    private JLabel equalsLbl;
+}
